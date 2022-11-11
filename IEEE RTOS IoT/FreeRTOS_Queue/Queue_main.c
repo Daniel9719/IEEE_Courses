92e1e08528a5bc4c4f9c949d9d7790ab95887ba7
Daniel Ortiz <danielortiz9719@gmail.com>
2022-11-11 10:19:10 -0600

    feat(FreeRTOS_Queue): Refactor code to default state

--- IEEE RTOS IoT/FreeRTOS_Queue/Queue_main.c
+++ IEEE RTOS IoT/FreeRTOS_Queue/Queue_main.c
@@ -14,16 +14,16 @@ void Task1 (void* args){
     struct Data_t PPG = {
         .ID_Val = "PPG",
         .Data_Val = 32
+        // .Data_Val = LOW
     }; 
     int i = 0;
     for(i=0; i<5; i++){
-        // Preguntar que va a suceder con prioridades iguales y diferentes
-        // UARTprintf("Tarea 1 manda valor %d\n", num);
-        UARTprintf("Tarea 1 manda valor %d de ID %s\n", PPG.Data_Val, PPG.ID_Val);
-        // xQueueSend(Queue1, &num, portMAX_DELAY);
-        xQueueSend(Queue1, &PPG, portMAX_DELAY);
-        // num++;
-        PPG.Data_Val++;
+        UARTprintf("Tarea 1 manda valor %d\n", num);
+        xQueueSend(Queue1, &num, portMAX_DELAY);
+        num++;
+        // UARTprintf("Tarea 1 manda valor %d de ID %s\n", PPG.Data_Val, PPG.ID_Val);
+        // xQueueSend(Queue1, &PPG, portMAX_DELAY);
+        // PPG.Data_Val++;
     }
     vTaskDelete(NULL);
 }
@@ -33,13 +33,14 @@ void Task2 (void* args){
     struct Data_t RxData;
     int i = 0;
     for(i=0; i<5; i++){
-        // xQueueReceive(Queue1, &num, portMAX_DELAY);
-        xQueueReceive(Queue1, &RxData, portMAX_DELAY);
-        // UARTprintf("Tarea 2 recibe valor %d\n", num);
-        UARTprintf("Tarea 2 recibe valor %d de ID %s\n", RxData.Data_Val, RxData.ID_Val);
-        num++;
-        xQueueOverwrite(Queue2, &RxData.Data_Val);
-        UARTprintf("Tarea 2 manda dato %d a buzon\n", num);
+        xQueueReceive(Queue1, &num, portMAX_DELAY);
+        UARTprintf("Tarea 2 recibe valor %d\n", num);
+        // xQueueReceive(Queue1, &RxData, portMAX_DELAY);
+        // UARTprintf("Tarea 2 recibe valor %d de ID %s\n", RxData.Data_Val, RxData.ID_Val);
+
+        // xQueueOverwrite(Queue2, &RxData.Data_Val);
+        // UARTprintf("Tarea 2 manda dato %d a buzon\n", num);
+        // num++;
     }
     vTaskDelete(NULL);
 }
@@ -49,18 +50,19 @@ void Task3 (void* args){
     struct Data_t EDA = {
         .ID_Val = "EDA",
         .Data_Val = 3
+        // .Data_Val = HIGH
     };
     
     while(1){
-        // Evidenciar uso de estructuras como miembros al no usarlas primero
-        // UARTprintf("Tarea 3 manda valor %d al frente de la cola\n", num);
-        UARTprintf("Tarea 3 manda valor %d al frente de la cola desde ID %s\n",
-                    EDA.Data_Val, EDA.ID_Val);
-        // xQueueSendToFront(Queue1, &num, portMAX_DELAY);
-        xQueueSendToFront(Queue1, &EDA, portMAX_DELAY);
+        UARTprintf("Tarea 3 manda valor %d al frente de la cola\n", num);
+        xQueueSendToFront(Queue1, &num, portMAX_DELAY);
+        // UARTprintf("Tarea 3 manda valor %d al frente de la cola desde ID %s\n",
+        //             EDA.Data_Val, EDA.ID_Val);
+        // xQueueSendToFront(Queue1, &EDA, portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(100));
-        xQueuePeek(Queue2, &num, portMAX_DELAY);
-        UARTprintf("Tarea 3 recibe valor %d del buzon\n", num);
+
+        // xQueuePeek(Queue2, &num, portMAX_DELAY);
+        // UARTprintf("Tarea 3 recibe valor %d del buzon\n", num);
     }
     vTaskDelete(NULL);
 }
--- IEEE RTOS IoT/FreeRTOS_Queue/Queue_main.h
+++ IEEE RTOS IoT/FreeRTOS_Queue/Queue_main.h
@@ -5,10 +5,22 @@
 
 typedef char* ID_t;
 
+enum SigLvl{
+    LOW = 0,
+    MEDIUM = 10,
+    HIGH = 30
+};
+
 struct Data_t
 {
     ID_t ID_Val;
     uint8_t Data_Val;
 };
+
+// struct Data_t
+// {
+//     ID_t ID_Val;
+//     enum SigLvl Data_Val;
+// };
 
 #endif
